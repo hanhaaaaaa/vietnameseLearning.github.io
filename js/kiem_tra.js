2898ffc0b053b45460d1d8c2de4e50fff19568a5
hanhaaaaaa <kpthanh26@gmail.com>
2020-02-16 23:48:45 +0700

    update

--- js/kiem_tra.js
+++ js/kiem_tra.js
@@ -1,7 +1,9 @@
 $(function(){
     $("#nop-bai").hide();
-    let m = 20; // Phút
-    let s = 0; // Giây
+    $("#toan-bo-cau-hoi").hide();
+    $(".cham-diem").hide();
+    let m = 0; // Phút
+    let s = 10; // Giây
     let timeout = null; // Timeout
     function dongHoDemGio(){
         /*BƯỚC 1: CHUYỂN ĐỔI DỮ LIỆU*/
@@ -12,19 +14,12 @@ $(function(){
             m -= 1;
             s = 59;
         }
-        // Nếu số phút = -1 tức là đã chạy ngược hết số phút, lúc này:
-        //  - giảm số giờ xuống 1 đơn vị
-        //  - thiết lập số phút lại 59
-        if (m === -1){
-            h -= 1;
-            m = 59;
-        }
-
         // Nếu số phút = -1 tức là đã hết giờ, lúc này:
         //  - Dừng chương trình
-        if (m == -1){
+        if (m === -1){
             clearTimeout(timeout);
-            alert('Hết giờ');
+            $("#het-thoi-gian").text("Hết thời gian rồi !!!!");
+            tinhDiem();
             return false;
         }
 
@@ -38,10 +33,52 @@ $(function(){
             dongHoDemGio();
         }, 1000);
     }
+    function dungdemthoigian(){
+        clearTimeout(timeout);
+    }
+    function tinhDiem(){
+        let diem = 0;
+        if($('input[name="cau1"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau2"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau3"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau4"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau5"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau6"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau7"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau8"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau9"]').val() === 'chết đứng') diem=diem+5;
+        if($('input[name="cau10"]').val() === 'tay mềm') diem=diem+5;
+        if($('input[name="cau11"]').val() === 'bỏ cỏ') diem=diem+5;
+        if($('input[name="cau10"]').val() === 'mài sắt') diem=diem+5;
+        if($('input[name="cau13"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau14"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau15"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau16"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau17"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau18"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau19"]:checked').val() === 'dung') diem=diem+5;
+        if($('input[name="cau20"]:checked').val() === 'dung') diem=diem+5;
+        $("#toan-bo-cau-hoi").hide();
+        $(".cham-diem").show();
+        if(diem >= 90){
+           $(".danh-gia").text("Wowww bạn thật xuất sắc!");
+        }else if(diem >= 80){
+            $(".danh-gia").text("Wowww bạn thật giỏi!");
+        }else if(diem >= 70){
+            $(".danh-gia").text("Bạn khá thật đấy!");
+        }
+        $("#diem").text(diem);
+    }
 
     $("#bat-dau").click(function(){
         $("#bat-dau").hide();
         $("#nop-bai").show();
+        $(".gioi-thieu").hide();
+        $("#toan-bo-cau-hoi").show();
         dongHoDemGio();
+    });
+    $("#nop-bai").click(function(){
+        tinhDiem();
+        dungdemthoigian();
     });
 });
