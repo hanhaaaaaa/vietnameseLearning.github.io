bc46716c22bc7e32a9d468540ca997278d8b61d7
hanhtx <kpthanh26@gmail.com>
2020-05-25 01:50:19 +0700

    update keo tha

--- js/cadao.js
+++ js/cadao.js
@@ -1,16 +1,36 @@
+$(function () {
+  $(".chuabai").hide();
+  $(".dung").hide();
+  $(".sai").hide();
+});
 function allowDrop(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+let arr = [];
+function drop(ev) {
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  arr.push(data);
+  console.log(arr.toString());
+}
+
+function ktcadao1() {
+  if (
+    arr.toString() === "co,cong,mai,sat,co2,ngay,nen,kim" ||
+    arr.toString() === "co2,cong,mai,sat,co,ngay,nen,kim"
+  ) {
+    $(".chuabai").show();
+    $(".dung").show();
+    $(".nut-click").hide();
+  } else {
+    $(".chuabai").show();
+    $(".sai").show();
+    $(".nut-click").hide();
   }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  let arr = [];
-  function drop(ev) {
-    ev.preventDefault();
-    let data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    arr.push(data)
-    console.log(arr)
-  }
+}
